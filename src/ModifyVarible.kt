38e4cc81aa7a1f845d61bfad001fdccaf142135d
priyanka khandge <priyakhandge10@gmail.com>
2020-02-13 18:13:13 +0530

    modify variable

---
+++ src/ModifyVarible.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var count = 1
+    println("Count: $count")
+    count = +1
+    println("Count: $count")
+    count = 2
+    println("Count: $count")
+    count = +4
+    println("Count: $count")
+
+}
