0498b6d727d099d90ada8f851d6a3e3ef196f45a
priyanka khandge <priyakhandge10@gmail.com>
2020-02-13 16:08:47 +0530

    Data types example

---
+++ src/DataTypes.kt
@@ -0,0 +1,19 @@
+fun main(){
+    val name = "Priyanka"
+    val age = 26
+    val place = "Thane"
+    var str: String
+    str= "priya"
+    var str1 = "Rahul"
+    var count = 1
+    println("Count: $count")
+    count = 10
+    println("Count: $count")
+    println("----------User Info----------")
+    println(str)
+    println(str1)
+    println("Name: $name")
+    println("Age: $age")
+    println("Place: $place")
+    println("Kotlin tutorial")
+}
