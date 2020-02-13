20d8596aee6ab7682f09b7a7ae0cd64104cd1672
priyanka khandge <priyakhandge10@gmail.com>
2020-02-13 16:49:12 +0530

    get user input

---
+++ src/InputData.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter Name:")
+    val name = readLine()!!.toString()
+    println("Enter  age:")
+    val age = readLine()!!.toInt()
+
+    println("------User Info-------")
+    println("your name is: $name")
+    println("your age: $age")
+
+}
