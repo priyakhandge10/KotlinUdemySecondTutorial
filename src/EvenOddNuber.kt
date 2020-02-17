d706fd61946caaa99e7ccc96687a3c43dac4f085
priyanka khandge <priyakhandge10@gmail.com>
2020-02-17 23:40:37 +0530

    Even odd number

---
+++ src/EvenOddNuber.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+    print("Enter the number:")
+
+    var number : Int = readLine()!!.toInt()
+
+    if(number % 2 == 0){
+
+        println("number is even")
+
+    }else{
+
+        println("number is odd")
+
+    }
+
+}
