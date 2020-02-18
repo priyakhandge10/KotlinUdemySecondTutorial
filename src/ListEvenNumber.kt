2a6093c7f01fe2a9ae5a7b5c8b3953bae7884ec6
priyanka khandge <priyakhandge10@gmail.com>
2020-02-18 14:32:51 +0530

    list of even number

---
+++ src/ListEvenNumber.kt
@@ -0,0 +1,14 @@
+
+    fun main(){
+
+        for(number:Int in 1..100){
+
+            if(number%2 ==0){
+
+                println("List of even number: $number")
+
+            }
+
+        }
+
+    }
