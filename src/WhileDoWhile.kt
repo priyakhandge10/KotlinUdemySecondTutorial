b4ed96218743edccda6aa3d70b72cf86f2a49d1c
priyanka khandge <priyakhandge10@gmail.com>
2020-02-18 13:06:02 +0530

    while dowhile loop

---
+++ src/WhileDoWhile.kt
@@ -0,0 +1,14 @@
+fun main(){
+println("---while loop---")
+    var i = 1
+    while (i<=10){
+        println("$i kotlin tutorial")
+        i  +=1
+    }
+    println("----do while------")
+    i = 1
+    do {
+        println("$i kotlin tutorial")
+        i +=2
+    }while (i<=10)
+}
