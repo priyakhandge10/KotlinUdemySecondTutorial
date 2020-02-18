264f20e4f56ca5f48444c331f63331e067df5431
priyanka khandge <priyakhandge10@gmail.com>
2020-02-18 12:30:45 +0530

    simple for loop

---
+++ src/SimpleFor.kt
@@ -0,0 +1,17 @@
+fun main(){
+    println("Start The App")
+    for (i: Int in 1..10){
+        println("$i Kotlin Tutorial")
+    }
+    println("Increment")
+    for (i: Int in 1..10 step 2){
+        println("$i kotlin tutorial")
+    }
+    println("Decrement")
+    for (i: Int in 10 downTo 1){
+        println("$i kotlin tutorial")
+    }
+    for (i: Int in 10 downTo 1 step 3){
+        println("$i kotlin tutorial")
+    }
+}
