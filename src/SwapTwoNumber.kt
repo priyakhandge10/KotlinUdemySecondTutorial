49b8617614f5d80ff7ef2c7a78cfa8d2bf32f17b
priyanka khandge <priyakhandge10@gmail.com>
2020-02-19 13:11:51 +0530

    swap two number with temp

---
+++ src/SwapTwoNumber.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    var x= 10
+
+    var y = 20
+    println("BEFORE SWAP:")
+    println("X: $x")
+    println("Y: $y")
+
+    var temp = x
+
+    x = y
+
+    y = temp
+    println("AFTER SWAP:")
+    println("X: $x")
+    println("Y: $y")
+
+}
