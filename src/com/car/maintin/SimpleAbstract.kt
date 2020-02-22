4ac1b8dfb2f18fc78b1fa54b525d0ec506bf1d6f
priyanka khandge <priyakhandge10@gmail.com>
2020-02-22 19:52:34 +0530

    simple abstract example

--- src/com/car/maintin/SimpleAbstract.kt
+++ src/com/car/maintin/SimpleAbstract.kt
@@ -1,2 +1,23 @@
 package com.car.maintin
 
+abstract class Credit(){
+    fun CreditID():String
+    { return "23432dsdfds"}
+    abstract fun newCredir()
+
+}
+
+class UserInfo(): Credit(){
+
+    fun getInfo():String{
+        return CreditID();
+    }
+    override  fun newCredir(){
+        println(" new cart created")
+    }
+}
+
+fun main(args:Array<String>){
+    var user=UserInfo()
+    println(user.getInfo())
+}
