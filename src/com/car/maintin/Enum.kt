6d20021ed926fa558cb603ddc12da94ad045dc09
priyanka khandge <priyakhandge10@gmail.com>
2020-02-22 19:51:34 +0530

    enum example

--- src/com/car/maintin/Enum.kt
+++ src/com/car/maintin/Enum.kt
@@ -1,2 +1,13 @@
 package com.car.maintin
 
+enum class Direction{
+    north, south, west, east
+}
+fun main(args: Array<String>){
+    var usedirection = Direction.east
+    if (usedirection == Direction.west){
+        println("yes")
+    }else{
+        println("no")
+    }
+}
