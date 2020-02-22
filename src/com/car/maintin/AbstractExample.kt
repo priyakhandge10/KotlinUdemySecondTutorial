1e518f816bcf139a3a1c65eaef9d7d046d6b4380
priyanka khandge <priyakhandge10@gmail.com>
2020-02-22 19:50:34 +0530

    my own abstract example

--- src/com/car/maintin/AbstractExample.kt
+++ src/com/car/maintin/AbstractExample.kt
@@ -1,2 +1,20 @@
 package com.car.maintin
 
+abstract class Teacher(name:String){
+    init {
+        println("my name is: $name")
+    }
+    abstract fun displayinfo(place:String)
+
+}
+
+class student(name: String): Teacher(name){
+    override fun displayinfo(place: String){
+    println("Place: $place")
+    }
+}
+
+fun main(args: Array<String>){
+    val stud = student("priyanka")
+    stud.displayinfo("thane")
+}
